9668018c0f570e37281724cbc7b5dc209df12b21
somicodes12 <somtochukwue98@gmail.com>
2023-10-06 22:24:33 +0100

    still working

--- socialFE/src/pages/auth/LandinScreen.tsx
+++ socialFE/src/pages/auth/LandinScreen.tsx
@@ -1,7 +1,19 @@
-
+import Device from "../../LandingComponents/Device"
+import LandingHero from "../../LandingComponents/LandingHero"
+import Review from "../../LandingComponents/Review"
+import Unique from "../../LandingComponents/Unique"
+// import LandingHeader from "../../LandingComponents/LandingHero"
+import FirstHeader from "../../components/static/FirstHeader"
+// FullStack2004
 const LandinScreen = () => {
   return (
-    <div>LandinScreen</div>
+    <div>
+       <FirstHeader/>
+       <LandingHero/>
+     <Unique/>
+     <Device/>
+     <Review/>
+    </div>
   )
 }
 
